78dc186d75cf1ffb96e36eac3d480cff631fc35c
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-06-18 12:54:09 +0900

    impl redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import "./shop.styles.scss";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategories } from "../../store/categories/category.action";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMapArray = await getCategoriesAndDocuments("categories");
+      console.log(categoryMapArray);
+      dispatch(setCategories(categoryMapArray));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />}></Route>
