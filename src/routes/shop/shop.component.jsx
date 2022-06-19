3b46498b6108dfb84e9a92b9d0bc1aec114c49b8
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-06-19 14:42:01 +0900

    redux-thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,17 +4,14 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import "./shop.styles.scss";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMapArray = await getCategoriesAndDocuments("categories");
-      console.log(categoryMapArray);
-      dispatch(setCategories(categoryMapArray));
+      dispatch(fetchCategoriesAsync());
     };
 
     getCategoriesMap();
